371850f9923d596356db9c350c0c4f23c0d05e55
Itzebren <brendaparrales36@gmail.com>
2026-04-26 11:02:53 -0600

    update: add fruit ninja placeholder colors

--- app/src/main/java/com/android/mobile/games/app/games/fruitninja/model/FruitNinjaItemType.kt
+++ app/src/main/java/com/android/mobile/games/app/games/fruitninja/model/FruitNinjaItemType.kt
@@ -1,5 +1,7 @@
 package com.android.mobile.games.app.games.fruitninja.model
 
+import androidx.compose.ui.graphics.Color
+
 enum class FruitNinjaItemType {
     APPLE,
     BANANA,
@@ -16,4 +18,16 @@ fun FruitNinjaItemType.isBomb(): Boolean {
 
 fun FruitNinjaItemType.isFruit(): Boolean {
     return this != FruitNinjaItemType.BOMB
+}
+
+fun FruitNinjaItemType.color(): Color {
+    return when (this) {
+        FruitNinjaItemType.APPLE -> Color.Red
+        FruitNinjaItemType.BANANA -> Color.Yellow
+        FruitNinjaItemType.WATERMELON -> Color.Green
+        FruitNinjaItemType.ORANGE -> Color(0xFFFF9800)
+        FruitNinjaItemType.PINEAPPLE -> Color(0xFFFFD54F)
+        FruitNinjaItemType.COCONUT -> Color(0xFF8D6E63)
+        FruitNinjaItemType.BOMB -> Color.DarkGray
+    }
 }
